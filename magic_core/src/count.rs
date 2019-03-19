8dcc87a9a20c7a7719f53a16654859015687b09a
Chris Gregory <czipperz@gmail.com>
2019-03-19 01:56:47 -0400

    Count: Add conversions from RangeInclusive and RangeFrom

--- magic_core/src/count.rs
+++ magic_core/src/count.rs
@@ -1,6 +1,6 @@
 /// The number of things to select.
 ///
-/// Maximum is exclusive.
+/// Maximum is inclusive.
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Count {
     pub minimum: usize,
@@ -13,5 +13,63 @@ impl From<usize> for Count {
             minimum: x,
             maximum: Some(x),
         }
+    }
+}
+
+use std::ops::{RangeFrom, RangeInclusive};
+impl From<RangeInclusive<usize>> for Count {
+    fn from(range: RangeInclusive<usize>) -> Count {
+        let (start, end) = range.into_inner();
+        Count {
+            minimum: start,
+            maximum: Some(end),
+        }
+    }
+}
+
+impl From<RangeFrom<usize>> for Count {
+    fn from(range: RangeFrom<usize>) -> Count {
+        Count {
+            minimum: range.start,
+            maximum: None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_range_from_usize() {
+        assert_eq!(
+            Count::from(2),
+            Count {
+                minimum: 2,
+                maximum: Some(2),
+            }
+        );
+    }
+
+    #[test]
+    fn test_range_inclusive() {
+        assert_eq!(
+            Count::from(0..=1),
+            Count {
+                minimum: 0,
+                maximum: Some(1),
+            }
+        );
+    }
+
+    #[test]
+    fn test_range_infinite() {
+        assert_eq!(
+            Count::from(1..),
+            Count {
+                minimum: 1,
+                maximum: None,
+            }
+        );
     }
 }
