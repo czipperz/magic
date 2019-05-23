9d7385d2d208f39908506332a33bd75a9cc9c046
Chris Gregory <czipperz@gmail.com>
2019-05-23 00:38:43 -0500

    Fix power and toughness being None in Benalish Hero and add construction test

--- magic_cards/src/creatures/humans/benalish_hero.rs
+++ magic_cards/src/creatures/humans/benalish_hero.rs
@@ -10,6 +10,25 @@ pub fn benalish_hero() -> Card {
         .with_type(Type::Creature)
         .with_subtype(Subtype::Human)
         .with_subtype(Subtype::Soldier)
+        .with_power(1)
+        .with_toughness(1)
         .with_keyword_ability(KeywordAbility::Bands)
         .build()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_constructor() {
+        let card = benalish_hero();
+        assert_eq!(card.name, "Benalish Hero");
+        assert_eq!(*card.mana_cost(), ManaCost::new().with_white(1));
+        assert_eq!(card.types, &[Type::Creature]);
+        assert_eq!(card.subtypes, &[Subtype::Human, Subtype::Soldier]);
+        assert_eq!(card.power, Some(1));
+        assert_eq!(card.toughness, Some(1));
+        assert_eq!(card.keyword_abilities, &[KeywordAbility::Bands]);
+    }
 }
